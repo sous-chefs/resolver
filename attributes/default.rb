2d9bed16ab61f3b0d71c3b399bfb248c7426b08a
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2012-06-26 23:17:32 +0000

    Support setting which file to use through an attribute.\nDefault to a reasonable default on modern ubuntus, which use resolvconf scripts to manage the /etc/resolv.conf file. See also http://www.stgraber.org/2012/02/24/dns-in-ubuntu-12-04/

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,3 +2,14 @@ default['resolver']['search'] = domain
 default['resolver']['nameservers'] = []
 default['resolver']['options'] = {}
 default['resolver']['server_role'] = 'nameserver'
+case platform
+when "ubuntu"
+  if platform_version.to_f <= 12.04
+    # append to the tail of resolvconf's generated files
+    default['resolver']['config_file'] = '/etc/resolvconf/resolv.conf.d/tail'
+  else
+    default['resolver']['config_file'] = '/etc/resolv.conf'
+  end
+else
+  default['resolver']['config_file'] = '/etc/resolv.conf'
+end
