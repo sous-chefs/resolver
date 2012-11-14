105ad0a2fc9c065a7c4455cfc07ab7771b8e0da0
jtimberman <joshua@opscode.com>
2012-11-13 23:08:15 -0700

    add name attribute to metadata

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
+name              "resolver"
 maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Configures /etc/resolv.conf via attributes"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.1.0"
+version           "1.1.1"
 
 recipe "resolver", "Configures /etc/resolv.conf via attributes"
 recipe "resolver::from_server_role", "Manages nameservers from role with explicitly set servers"
