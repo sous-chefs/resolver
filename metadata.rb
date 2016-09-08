d37a8a69125b189042302c9a1b5c98ab57611a4f
Tim Smith <tsmith@chef.io>
2016-09-07 21:16:51 -0700

    Require chef 12+
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -13,5 +13,5 @@ recipe 'resolver::from_server_role', 'Manages nameservers from role with explici
   supports os
 end
 
-source_url 'https://github.com/chef-cookbooks/resolver' if respond_to?(:source_url)
-issues_url 'https://github.com/chef-cookbooks/resolver/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/chef-cookbooks/resolver'
+issues_url 'https://github.com/chef-cookbooks/resolver/issues'
