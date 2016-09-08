10a251c434564589c768a244b62761c811e2a4b5
Tim Smith <tsmith@chef.io>
2016-09-07 21:26:37 -0700

    Release 2.0.0
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  'cookbooks@chef.io'
 license           'Apache 2.0'
 description       'Configures /etc/resolv.conf via attributes'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '1.3.1'
+version '2.0.0'
 
 recipe 'resolver', 'Configures /etc/resolv.conf via attributes'
 recipe 'resolver::from_server_role', 'Manages nameservers from role with explicitly set servers'
@@ -15,3 +15,5 @@ end
 
 source_url 'https://github.com/chef-cookbooks/resolver'
 issues_url 'https://github.com/chef-cookbooks/resolver/issues'
+
+chef_version '>= 12.1'
