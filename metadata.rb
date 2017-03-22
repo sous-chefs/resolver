4ab8cb0dfee3696fb8a1e4398e95bff9f33fd6ab
Tim Smith <tsmith@chef.io>
2017-03-21 21:04:59 -0700

    Update Apache license string in metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name              'resolver'
 maintainer        'Chef Software, Inc.'
 maintainer_email  'cookbooks@chef.io'
-license           'Apache 2.0'
+license           'Apache-2.0'
 description       'Configures /etc/resolv.conf via attributes'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.0.1'
