ad59ca6a25ac592c0e85df9c3de9cc4a1de95104
Steve Clark <steve@bigsteve.us>
2018-02-12 09:34:46 -0500

    add aix support
    Signed-off-by: Steve Clark <steve@bigsteve.us>

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,11 @@ maintainer_email  'cookbooks@chef.io'
 license           'Apache-2.0'
 description       'Configures /etc/resolv.conf via attributes'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.0.1'
+version '2.0.2'
 
 recipe 'resolver', 'Configures /etc/resolv.conf via attributes'
 
-%w(ubuntu debian fedora centos redhat oracle scientific amazon freebsd openbsd mac_os_x solaris2 opensuse opensuseleap suse).each do |os|
+%w(aix ubuntu debian fedora centos redhat oracle scientific amazon freebsd openbsd mac_os_x solaris2 opensuse opensuseleap suse).each do |os|
   supports os
 end
 
