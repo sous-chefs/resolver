be6fba58f894e3f2065e942701ad46f70ac30785
Tim Smith <tsmith84@gmail.com>
2019-11-10 17:48:30 -0800

    Remove long_description and recipe metadata
    No need for these anymore
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,10 +3,7 @@ maintainer        'Chef Software, Inc.'
 maintainer_email  'cookbooks@chef.io'
 license           'Apache-2.0'
 description       'Configures /etc/resolv.conf via attributes'
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.1.0'
-
-recipe 'resolver', 'Configures /etc/resolv.conf via attributes'
 
 %w(aix ubuntu debian fedora centos redhat oracle scientific amazon freebsd openbsd mac_os_x solaris2 opensuse opensuseleap suse).each do |os|
   supports os
