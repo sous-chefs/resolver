1199af5e4e57679c756764f55a5da9fe7c6b02cf
Ryan <kennedy@osuosl.org>
2021-02-09 16:10:37 -0800

    Add additional testing

--- test/integration/default/inspec/default_spec.rb
+++ test/integration/default/inspec/default_spec.rb
@@ -4,3 +4,7 @@ describe file('/etc/resolv.conf') do
   its('content') { should match /^nameserver 8\.8\.8\.8/ }
   its('content') { should match /^options rotate timeout:2/ }
 end
+
+describe command('curl https://www.google.com') do
+  its('exit_status') { should eq 0 }
+end
