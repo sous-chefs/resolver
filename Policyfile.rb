d596fc7eb0f9c3f23f6efeb800680e7989e6d363
Dan Webb <dan.webb@damacus.io>
2026-07-01 10:50:11 +0100

    fix: select systemd-resolved policyfile run list

--- Policyfile.rb
+++ Policyfile.rb
@@ -9,6 +9,9 @@ cookbook 'resolver-test', path: './test/integration/cookbooks/resolver-test'
 
 Dir.children('./test/integration/cookbooks/resolver-test/recipes').grep(/\.rb\z/).sort.each do |recipe|
   recipe_name = File.basename(recipe, '.rb')
+  next if recipe_name == 'systemd_resolved'
 
   named_run_list recipe_name.to_sym, 'resolver-test::' + recipe_name
 end
+
+named_run_list :systemd_resolved, 'resolver-test::test_packages', 'resolver-test::systemd_resolved'
